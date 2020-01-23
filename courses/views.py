550ed0c639d90ed413c85f50cc632ac99f12f2c5
Durval Carvalho <durvalcsouza@outlook.com>
2020-01-23 19:07:06 -0300

    Add CRUD to courses

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
-# Create your views here.
+from . import mixins
+from .models import Course
+
+
+class ManageCourseListView(mixins.OwnerCourseMixin, ListView):
+    template_name = 'courses/manage/course/list.html'
+
+
+class CourseCreateView(PermissionRequiredMixin,
+                       mixins.OwnerCourseEditMixin,
+                       CreateView):
+    permission_required = 'courses.add_course'
+
+
+class CourseUpdateView(PermissionRequiredMixin,
+                       mixins.OwnerCourseEditMixin,
+                       UpdateView):
+    permission_required = 'courses.change_course'
+
+
+class CourseDeleteView(PermissionRequiredMixin,
+                       mixins.OwnerCourseMixin,
+                       DeleteView):
+    template_name = 'courses/manage/course/delete.html'
+    success_url = reverse_lazy('manage_course_list')
+    permission_required = 'courses.delete_course'
