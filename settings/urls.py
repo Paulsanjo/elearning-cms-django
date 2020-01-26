75bd7746bc2ae9e695f70de08ab2bee1b2adcc47
Durval Carvalho <durvalcsouza@outlook.com>
2020-01-26 17:58:23 -0300

    Add landing page

--- settings/urls.py
+++ settings/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
+from courses.views import CourseListView
 
 
 urlpatterns = [
@@ -17,6 +18,8 @@ urlpatterns = [
     ),
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
+
+    path('', CourseListView.as_view(), name='course_list'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
